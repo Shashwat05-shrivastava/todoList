1adeb25874257ccff180167aa58c9069de34bb27
shashwat kumar shrivastava <shashwatkumar05@gmail.com>
2022-01-24 19:56:43 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import {Todo} from "./todo";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'firstpage';
+
+  todoValue!:string;
+  list!:Todo[];
+
+  ngOnInit(){
+    this.list=[];
+    this.todoValue="";
+  }
+  addItem(){
+    if(this.todoValue !== ""){
+      const newItem:Todo={
+        id:Date.now(),
+        value:this.todoValue,
+        isDone:false
+      };
+      this.list.push(newItem);
+    }
+    this.todoValue="";
+  }
+  deleteItem(id:number){
+    this.list=this.list.filter( item =>item.id !== id);
+  }
 }
